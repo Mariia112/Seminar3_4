5846cf60ca4519acf7b111aaa051309bd4a11de5
Mariia <martush1992@gmail.com>
2022-09-16 14:01:17 +0300

    Finish

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,20 +2,21 @@
 
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
- 
-  Console.Write("Введите число   : ");
-  int number = int.Parse(Console.ReadLine());
-  
-  Console.Write("Введите степень : ");
-  int n = int.Parse(Console.ReadLine());
-  
-  
-  int number_n=1;
-  for(int i=0; i<n; i++) {
-   number_n*=number;
-  }
-  Console.WriteLine("{0} ^ {1} = {2}", number, n, number_n);
-  Console.ReadKey();
-  return 0;
- 
+
+Console.Write("Введите число   : ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень : ");
+int n = int.Parse(Console.ReadLine());
+
+
+int number_n = 1;
+for (int i = 0; i < n; i++)
+{
+    number_n *= number;
+}
+Console.WriteLine("{0} ^ {1} = {2}", number, n, number_n);
+Console.ReadKey();
+return 0;
+
 
