ff684e1a497d3327e62093c202941d7c4045c400
Mariia <martush1992@gmail.com>
2022-09-16 12:19:17 +0300

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+//достаем цифры из числа в обратном порядке
+Console.Write("Введите число: ");
+int numbers = int.Parse(Console.ReadLine()!);
+int sum = 0;
+
+while (numbers > 0)
+{
+    int digit = numbers % 10;
+    sum += digit;
+    numbers = numbers / 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе - {sum}");
